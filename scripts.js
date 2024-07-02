e35abef58d164fc1c2f6c2dde9c61ae148b60d40
Nguyen Trang <167010544+gobvnanas@users.noreply.github.com>
2024-07-02 07:43:45 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+function filterSelection(category) {
+    var images = document.getElementsByClassName("filterDiv");
+    if (category === "all") category = "";
+    for (var i = 0; i < images.length; i++) {
+        images[i].classList.remove("show");
+        if (images[i].className.indexOf(category) > -1) {
+            images[i].classList.add("show");
+        }
+    }
+}
+
+// Show all images by default when the page loads
+filterSelection('all');
+
+// Make the filter div draggable
+dragElement(document.getElementById("filter"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Get the mouse cursor position at startup
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // Call a function whenever the cursor moves
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Calculate the new cursor position
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Set the element's new position
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // Stop moving when mouse button is released
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
