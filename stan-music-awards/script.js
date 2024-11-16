dac00293f2f2e93954290ea6dd0e4ed59d66d0d9
Nguyen Trang <167010544+gobvnanas@users.noreply.github.com>
2024-11-16 22:10:20 +0700

    script.js

--- stan-music-awards/script.js
+++ stan-music-awards/script.js
@@ -1,4 +1,3 @@
-
 function showTabContent(tab) {
     document.querySelectorAll('.tab-content').forEach(function (content) {
         content.style.display = 'none';
@@ -8,6 +7,16 @@ function showTabContent(tab) {
   
   document.addEventListener('DOMContentLoaded', function () {
     showTabContent('projects');
+  });
+  function changeTab(tab, event) {
+    event.preventDefault();
+    showTabContent(tab);
+    const newUrl = `/${tab}`;
+    window.history.pushState({ tab }, '', newUrl);
+  }
+  window.addEventListener('popstate', function (event) {
+    if (event.state && event.state.tab) {
+        showTabContent(event.state.tab); }
   });
   
   function toggleDropdown() {
@@ -84,96 +93,13 @@ function showTabContent(tab) {
   )[1];
   }
   
-  document.addEventListener('DOMContentLoaded',function(event){
-  var dataText = [ 
-      "> Nguyen Trang is a graphic / brand designer based in HCMC, specializing in brand identity design. her portfolio, gobvnanas, showcases her selected projects.",
-      ];
-  function typeWriter(text, i, fnCallback) {
-  if (i < (text.length)) {
-  document.querySelector("p").innerHTML = text.substring(0, i+1) +'<em aria-hidden="true"></em>';
-  
-  setTimeout(function() {
-      typeWriter(text, i + 1, fnCallback)
-  }, 100);
-  }
-  else if (typeof fnCallback == 'function') {
-  setTimeout(fnCallback, 100);
-  }
-  }
-  function StartTextAnimation(i) {
-  if (typeof dataText[i] == 'undefined'){
-      setTimeout(function() {
-          StartTextAnimation(0);
-      }, 20000);
-  }
-  if (i < dataText[i].length) {
-  typeWriter(dataText[i], 0, function(){
-     StartTextAnimation(i + 1);
-  });
-  }
-  }
-  StartTextAnimation(0);
-  });
-  
-  
-  let activeFilters = [];
-  
-  function filterImages() {
-      let images = document.querySelectorAll('.filter');
-  
-      images.forEach(function(image) {
-          if (activeFilters.length === 0 || activeFilters.includes('all')) {
-              image.style.display = 'block';
-          } else {
-              let imageClasses = Array.from(image.classList);
-              let match = activeFilters.some(filter => imageClasses.includes(filter));
-  
-              image.style.display = match ? 'block' : 'none';
-          }
-      });
-  }
-  
-  document.querySelectorAll('.dropdown-content a').forEach(function(link) {
-      link.addEventListener('click', function(e) {
-          e.preventDefault();
-  
-          let filter = this.dataset.filter;
-  
-          if (filter === 'all') {
-              activeFilters = ['all'];
-          } else {
-              let filterIndex = activeFilters.indexOf(filter);
-  
-              if (filterIndex > -1) {
-                  activeFilters.splice(filterIndex, 1);
-              } else {
-                  activeFilters = activeFilters.filter(f => f !== 'all');
-                  activeFilters.push(filter);
-              }
-          }
-          document.querySelectorAll('.dropdown-content a').forEach(function(item) {
-              item.classList.remove('active');
-          });
-          activeFilters.forEach(f => {
-              document.querySelector(`[data-filter="${f}"]`).classList.add('active');
-          });
-          filterImages();
-      });
-  });
-  
-  
   function infiniteScroll(column) {
   column.addEventListener('scroll', () => {
     if (column.scrollTop + column.clientHeight >= column.scrollHeight - 5) {
       column.innerHTML += column.innerHTML;
-    }
-  });
-  }
-  
+    }  }); }
   document.addEventListener("DOMContentLoaded", function() {
   const column1 = document.getElementById('column-a');
   const column2 = document.getElementById('column-b');
-  
   infiniteScroll(column1);
-  infiniteScroll(column2);
-  });
+  infiniteScroll(column2); });
